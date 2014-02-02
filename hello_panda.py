130951a6719e556b4a5ead89aeec128a5f6877c0
Den Lesnov <den-lesnov@yandex-team.ru>
2014-02-03 01:53:44 +0400

    Load terrain. Magic!

--- hello_panda.py
+++ hello_panda.py
@@ -6,6 +6,11 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        self.environ = self.loader.loadModel('models/environment')
+        self.environ.reparentTo(self.render)
+        self.environ.setScale(.25, .25, .25)
+        self.environ.setPos(-8, 42, 0)
+
 
 if __name__ == '__main__':
     app = MyApp()
