669a9b3dd914b249e7becbfd5d054098552edfad
Den Lesnov <den-lesnov@yandex-team.ru>
2014-02-04 00:08:06 +0400

    Panda walks!

--- hello_panda.py
+++ hello_panda.py
@@ -3,12 +3,16 @@ from math import pi, sin, cos
 from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 
 class MyApp(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+
+        self.disableMouse()
 
         self.environ = self.loader.loadModel('models/environment')
         self.environ.reparentTo(self.render)
@@ -22,11 +26,28 @@ class MyApp(ShowBase):
         self.panda.reparentTo(self.render)
         self.panda.loop('walk')
 
+        point_a = Point3(0, -10, 0)
+        point_b = Point3(0, 10, 0)
+        point_c = Point3(180, 0, 0)
+        point_d = Point3(0, 0, 0)
+
+        self.panda_pace = Sequence(
+            self.panda.posInterval(3,
+                                   point_a,
+                                   startPos=point_b),
+            self.panda.hprInterval(1, point_c, startHpr=point_d),
+            self.panda.posInterval(3,
+                                   point_b,
+                                   startPos=point_a),
+            self.panda.hprInterval(1, point_d, startHpr=point_c),
+            name='panda_pace'
+        )
+        self.panda_pace.loop()
 
     def spin_camera_task(self, task):
         angle_deg = task.time * 6.0
         angle_rad = angle_deg * (pi / 180.0)
-        self.camera.setPos(20 * sin(angle_rad), -20.0 * cos(angle_rad), 3)
+        self.camera.setPos(30 * sin(angle_rad), -40.0 * cos(angle_rad), 3)
         self.camera.setHpr(angle_deg, 0, 0)
         return Task.cont
 
